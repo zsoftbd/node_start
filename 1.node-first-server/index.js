5d95ceac293a87f28a256e22702c3a56bd8a6e6b
zsoftbd <mzeaulkarim811@gmail.com>
2023-07-01 11:11:17 +0600

    content added

---
+++ 1.node-first-server/index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const app=express();
+
+
+app.get('/', (req, res)=>{
+        res.send('Welcome');
+})
+app.get('/home', (req, res)=>{
+        res.send('Welcome');
+})
+app.listen(3000,()=>{
+    console.log('listening on 3000');
+});
